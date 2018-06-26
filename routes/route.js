e373ccf7477ba58b41c18cf79b2bb1d7e0e627af
Handrew <andrew.a.han@gmail.com>
2018-06-25 23:16:35 -0400

    finished the routes.js. and now need to test

--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,32 @@ module.exports = function(app) {
     app.get("/auth/check", (req, res) => {
         if (req.user) {
             res.json({ user: req.user });
-        }
-    })
+        } else {
+            res.json(false);
+        };
+    });
+
+    app.post("/auth/login", passport.authentication('local'), (req, res) => {
+        res.json(true);
+    });
+
+    app.post("/auth/register", (req, res) => {
+        User.register(new User({
+            username: req.body.username
+        }), req.body.password, (err, user) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            passport.authenticate("local")(req, res, () => {
+                res.json(true);
+            });
+        });
+    });
+
+    app.get("/auth/logout", (req, res) => {
+        req.logout();
+        res.json(true);
+    });
+
 };
